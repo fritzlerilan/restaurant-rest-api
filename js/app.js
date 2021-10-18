e3b623e61ed246444bbbec910766967b9ed59d38
Ilan Fritzler <fritzlerilan@gmail.com>
2021-10-18 09:00:04 -0300

    show alert invalid data

--- js/app.js
+++ js/app.js
@@ -1,22 +1,34 @@
 let cliente = {
-  mesa: "",
-  hora: "",
-  pedido: [],
+    mesa: "",
+    hora: "",
+    pedido: [],
 };
 
 const btnGuardarCliente = document.querySelector("#guardar-cliente");
-btnGuardarCliente.addEventListener('click', guardarCliente);
+btnGuardarCliente.addEventListener("click", guardarCliente);
 
-function guardarCliente(){
-    const mesa = document.querySelector('#mesa').value
-    const hora = document.querySelector('#hora').value
+function guardarCliente() {
+    const mesa = document.querySelector("#mesa").value;
+    const hora = document.querySelector("#hora").value;
 
     // Revisar si hay campos vacios
-    const camposVacios = [mesa, hora].some(campo => campo === '')
+    const camposVacios = [mesa, hora].some((campo) => campo === "");
 
-    if(camposVacios) {
-        console.log('Si hay al menos un campo vacio')
-    }else {
-        console.log('Todos los campos estan llenos')
+    if (camposVacios) {
+        // Verificar si ya existe alerta
+        const existeAlerta = document.querySelector(".invalid-feedback");
+
+        if (!existeAlerta) {
+            const alerta = document.createElement("div");
+            alerta.classList.add("invalid-feedback", "d-block", "text-center");
+            alerta.textContent = "Todos los campos son obligatorios";
+            document.querySelector(".modal-body form").appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+        return;
     }
+    
 }
